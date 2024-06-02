ed70e30b0339099e7cbb1adfeb4e05ccce4c89e7
Vivek Pathak <vivekpathak8198@gmail.com>
2024-06-02 13:45:02 +0530

    Create Navbar and About Components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "styled-components";
+import styled from "styled-components";
+import { BrowserRouter as Router } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { darkTheme, lightTheme } from "./utils/Themes";
+//import About from "./components/about";
+import Navbar from "./components/navbar/Navbar";
+
+// const body = styled.div`
+//   background-color: ${({ theme }) => theme.bg};
+//   width: 100%;
+//   overflow-x: hidden;
+// `;
+
+// const Wrapper = styled.div`
+//   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
+//   width: 100%;
+//   clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
+// `
 
 function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  const [openModal, setOpenModal] = useState({ state: false, project: null });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Router>
+        <Navbar />
+      </Router>
+    </ThemeProvider>
   );
 }
 

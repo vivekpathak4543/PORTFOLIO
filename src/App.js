2203274593c8a53cea457d0c2884fb58ceb7b46a
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-10 00:38:15 +0530

    other updated files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,37 @@
 import { ThemeProvider } from "styled-components";
 import styled from "styled-components";
 import { BrowserRouter as Router } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { darkTheme, lightTheme } from "./utils/Themes";
-//import About from "./components/about";
 import Navbar from "./components/navbar/Navbar";
+import HeroSection from "./components/heroSection/HeroSection";
+import Footer from "./components/footer/Footer";
+import Contact from "./components/contact/Contact";
+import Skills from "./components/skills/Skills";
+import Project from "./components/project/Project";
+import Experience from "./components/experience/Experience";
+import ProjectDetails from "./components/projectDetails/ProjectDetails";
 
-// const body = styled.div`
-//   background-color: ${({ theme }) => theme.bg};
-//   width: 100%;
-//   overflow-x: hidden;
-// `;
+const Body = styled.div`
+  background-color: ${({ theme }) => theme.bg};
+  width: 100%;
+  overflow-x: hidden;
+`;
 
-// const Wrapper = styled.div`
-//   background: linear-gradient(38.73deg, rgba(204, 0, 187, 0.15) 0%, rgba(201, 32, 184, 0) 50%), linear-gradient(141.27deg, rgba(0, 70, 209, 0) 50%, rgba(0, 70, 209, 0.15) 100%);
-//   width: 100%;
-//   clip-path: polygon(0 0, 100% 0, 100% 100%,30% 98%, 0 100%);
-// `
+const Wrapper = styled.div`
+  background: linear-gradient(
+      38.73deg,
+      rgba(204, 0, 187, 0.15) 0%,
+      rgba(201, 32, 184, 0) 50%
+    ),
+    linear-gradient(
+      141.27deg,
+      rgba(0, 70, 209, 0) 50%,
+      rgba(0, 70, 209, 0.15) 100%
+    );
+  width: 100%;
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 30% 98%, 0 100%);
+`;
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -25,6 +40,21 @@ function App() {
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <Router>
         <Navbar />
+        <Body>
+          <HeroSection />
+          <Wrapper>
+            <Skills />
+            <Experience />
+          </Wrapper>
+          <Project openModal={openModal} setOpenModal={setOpenModal} />
+          <Wrapper>
+            <Contact />
+          </Wrapper>
+          <Footer />
+          {openModal.state && (
+            <ProjectDetails openModal={openModal} setOpenModal={setOpenModal} />
+          )}
+        </Body>
       </Router>
     </ThemeProvider>
   );

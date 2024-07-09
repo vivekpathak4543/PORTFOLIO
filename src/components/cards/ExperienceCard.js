83cca52d369b910d6ccdf8fc3d60990307b047b0
Vivek Pathak <vivekpathak8198@gmail.com>
2024-07-10 00:24:29 +0530

    add ExperienceCard component for displaying job experiences

---
+++ src/components/cards/ExperienceCard.js
@@ -0,0 +1,54 @@
+import React from "react";
+import {
+  Document,
+  Description,
+  Span,
+  Card,
+  Top,
+  Image,
+  Body,
+  Role,
+  Company,
+  Date,
+  Skills,
+  ItemWrapper,
+  Skill,
+} from "./ExperienceCardStyled";
+
+const ExperienceCard = ({ experience }) => {
+  return (
+    <Card>
+      <Top>
+        <Image src={experience.img} />
+        <Body>
+          <Role>{experience.role}</Role>
+          <Company>{experience.company}</Company>
+          <Date>{experience.date}</Date>
+        </Body>
+      </Top>
+      <Description>
+        {experience?.desc && <Span>{experience?.desc}</Span>}
+        {experience?.skills && (
+          <>
+            <br />
+            <Skills>
+              <b>Skills:</b>
+              <ItemWrapper>
+                {experience?.skills?.map((skill, index) => (
+                  <Skill key={index}> {skill}</Skill>
+                ))}
+              </ItemWrapper>
+            </Skills>
+          </>
+        )}
+      </Description>
+      {experience.doc && (
+        <a href={experience.doc} target="new">
+          <Document src={experience.doc} />
+        </a>
+      )}
+    </Card>
+  );
+};
+
+export default ExperienceCard;
